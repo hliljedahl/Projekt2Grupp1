e7cb4ac4c0683fd30e913190477e2793199f6e2f
H Liljedahl <hliljed@kth.se>
2018-04-05 16:20:15 +0200

    update
    //Hampus

--- Server/remove_data.php
+++ Server/remove_data.php
@@ -2,12 +2,20 @@
     // Connect to MySQL
     include("connect.php");
 
+
+
+    //MAX NUMBER OF ROWS
+    $max = 10;
+
+
     // Prepare the SQL statement
     //Count thee number of rows
     $sql_count = "SELECT * FROM data01";
     $sql_res = mysqli_query($conn, $sql_count);
     $numrows = mysqli_num_rows($sql_res);
-    $sql_remove = $numrows - 10;
+    $sql_remove = $numrows - $max;
+
+    //System Print
     echo nl2br ("\n" . $numrows);
     echo nl2br ("\n" . $sql_remove);
 
@@ -35,7 +43,12 @@
 
     }
 
+    //ADD
     //http://www.lonelycircuits.se/data/add_data.php?temp=XX&humi=XX&id=XX
     //http://www.lonelycircuits.se/data/add_data.php?temp=24&humi=12&id=8
+    //REMOVE
+    //http://lonelycircuits.se/data/remove_data.php
+    //DISPLAY
+    //http://lonelycircuits.se/data/index.php
 
 ?>
