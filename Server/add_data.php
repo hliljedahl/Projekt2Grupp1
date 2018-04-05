a3e32e713e96ef9ce30a8c9b4621e05a130cca51
H Liljedahl <hliljed@kth.se>
2018-04-05 14:50:59 +0200

    update
    //Hampus

--- Server/add_data.php
+++ Server/add_data.php
@@ -3,16 +3,17 @@
     include("connect.php");
 
     // Prepare the SQL statement
-      date_default_timezone_set('Europe/Stockholm');
-     $dateS = date('d/m/Y h:i:s', time());
+    date_default_timezone_set('Europe/Stockholm');
+    $dateS = date('d/m/Y h:i:s', time());
     echo $dateS;
-//    $SQL = "INSERT INTO yourdatabasename.data (time,temp,humi,test) VALUES ('$dateS','".$_GET["temp"]."','".$_GET["humi"]."','".$_GET["test"]."')";
+
+    //$SQL = "INSERT INTO yourdatabasename.data (time,temp,humi,test) VALUES ('$dateS','".$_GET["temp"]."','".$_GET["humi"]."','".$_GET["test"]."')";
     //$sql_in = "INSERT INTO test (time,temp,humi,id) VALUES ('$dateS','33.2','13.3','4')";
     $sql_in = "INSERT INTO data01 (time,temp,humi,id) VALUES ('$dateS','".$_GET["temp"]."','".$_GET["humi"]."','".$_GET["id"]."')";
+    
 
 
-
-    //http://www.lonelycircuits.se/add_data.php?temp="+temp+"&humi="+humi+"&id="+id
+    //http://www.lonelycircuits.se/add_data.php?temp=XX&humi=XX&id=XX
 
 
     // Execute SQL statement
